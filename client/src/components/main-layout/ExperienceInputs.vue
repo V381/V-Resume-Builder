c43611c4e26f45b547293d9fbd3f710bb7b75d9d
V381 <paavlee@yahoo.com>
2024-03-20 10:33:30 +0100

    Dynamic components

--- client/src/components/main-layout/ExperienceInputs.vue
+++ client/src/components/main-layout/ExperienceInputs.vue
@@ -1,37 +1,21 @@
 <template>
   <div>
-    <form v-for="(experience, index) in items" :key="experience.id" @submit.prevent>
-      <p class="organization-title">{{ $t('organizationCompany', { index: index + 1 }) }}</p>
-      <label>
-        {{ $t('organizationCompany') }}
-        <Input v-model="experience.organization" :placeholder="$t('organizationCompany')" />
-      </label>
-      <label>
-        {{ $t('organizationTitle') }}
-        <Input v-model="experience.title" :placeholder="$t('organizationTitle')" />
-      </label>
-      <label>
-        {{ $t('duration') }}
-        <Input v-model="experience.duration" :placeholder="$t('duration')" />
-      </label>
-      <label>
-        {{ $t('description') }}
-        <Input v-model="experience.description" :placeholder="$t('description')" />
-      </label>
-      <button class="remove-button" @click="() => remove(experience)">
-        {{ $t('removeExperience') }}
-      </button>
-    </form>
-    <div class="container">
-      <button @click="add" class="center-button">{{ $t('addExperience') }}</button>
-    </div>
+    <CustomForm 
+      :items="items"
+      organizationTitleKey="organizationCompany"
+      titleKey="organizationTitle"
+      durationKey="duration"
+      descriptionKey="description"
+      removeKey="remove"
+      addKey="add"
+    />
   </div>
 </template>
 
 <script setup>
 import { useExperienceStore } from "@/stores/storeTemplate";
-import Input from "@/components/main-layout/Input.vue";
 import useStoreItems from "@/helpers/useStoreItems";
+import CustomForm from "./CustomForm.vue";
 
 const { items, add, remove } = useStoreItems(
   useExperienceStore,
@@ -39,7 +23,6 @@ const { items, add, remove } = useStoreItems(
   'removeItem'
 );
 </script>
-
 
 <style scoped>
 .organization-title {
--- client/src/stores/storeTemplate.js
+++ client/src/stores/storeTemplate.js
@@ -1,5 +1,28 @@
 import { defineStore } from 'pinia';
 
+export const useStoreItems = (id) => {
+    const store = createStoreTemplate(id);
+    const items = ref(store.getItems());
+  
+    const add = (item) => {
+      store.addItem(item);
+    };
+  
+    const remove = (item) => {
+      store.removeItem(item);
+    };
+  
+    watch(
+      () => store.getItems(),
+      (newItems) => {
+        items.value = newItems;
+        console.log('Items updated:', newItems);
+      }
+    );
+  
+    return { items, add, remove };
+  };
+
 export const createStoreTemplate = (id) => {
   return defineStore({
     id,
