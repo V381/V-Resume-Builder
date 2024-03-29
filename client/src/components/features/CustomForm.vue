5db1571dd8837fc174ab725103c46238af80e474
V381 <paavlee@yahoo.com>
2024-03-29 01:56:21 +0100

    reseting head to previous

--- client/src/components/features/CustomForm.vue
+++ client/src/components/features/CustomForm.vue
@@ -2,48 +2,101 @@
   <div>
     <form v-if="formType === 'experience'" v-for="(experience, index) in items" :key="experience.id" @submit.prevent>
       <p class="organization-title">{{ $t('organization', { index: index + 1 }) }}</p>
-      <label>
-        {{ $t('organizationCompany') }}
-        <Input v-model="experience.organization" :placeholder="$t('organizationCompany')" />
-      </label>
-      <label>
-        {{ $t('organizationTitle') }}
-        <Input v-model="experience.title" :placeholder="$t('organizationTitle')" />
-      </label>
-      <label>
-        {{ $t('duration') }}
-        <Input v-model="experience.duration" :placeholder="$t('duration')" />
-      </label>
-      <label>
-        {{ $t('description') }}
-        <Input v-model="experience.description" :placeholder="$t('description')" />
-      </label>
+      <CustomLabel
+        :inputId="`organization-${index}`"
+        :labelText="$t('organizationCompany')"
+        :errorMessage="organizationError">
+        <Input
+          :id="`organization-${index}`"
+          v-model="experience.organization"
+          :placeholder="$t('organizationCompany')" />
+      </CustomLabel>
+
+      <CustomLabel
+        :inputId="`title-${index}`"
+        :labelText="$t('organizationTitle')"
+        :errorMessage="titleError">
+        <Input
+          :id="`title-${index}`"
+          v-model="experience.title"
+          :placeholder="$t('organizationTitle')" />
+      </CustomLabel>
+
+      <CustomLabel
+        :inputId="`duration-${index}`"
+        :labelText="$t('duration')"
+        :errorMessage="durationError">
+        <Input
+          :id="`duration-${index}`"
+          v-model="experience.duration"
+          :placeholder="$t('duration')" />
+      </CustomLabel>
+
+      <CustomLabel
+        :inputId="`description-${index}`"
+        :labelText="$t('description')"
+        :errorMessage="descriptionError">
+        <Input
+          :id="`description-${index}`"
+          v-model="experience.description"
+          :placeholder="$t('description')" />
+      </CustomLabel>
+
       <button class="remove-button" @click="remove(experience)">
         {{ $t('removeExperience') }}
       </button>
-    </form>
+
+    </form>    
     <div v-if="formType === 'experience'" class="container">
       <button @click="add('experience')" class="center-button">{{ $t('addExperience') }}</button>
     </div>
-    
+
+
     <form v-if="formType === 'education'" v-for="(education, index) in items" :key="education.id" @submit.prevent>
       <p class="school-title">{{ $t('school', { index: index + 1 }) }}</p>
-      <label>
-        {{ $t('school') }}
-        <Input v-model="education.name" :placeholder="$t('school')" />
-      </label>
-      <label>
-        {{ $t('from') }}
-        <Input v-model="education.from" type="date" :placeholder="$t('from')" />
-      </label>
-      <label>
-        {{ $t('to') }}
-        <Input v-model="education.to" type="date" :placeholder="$t('to')" />
-      </label>
-      <label>
-        {{ $t('educationTitle') }}
-        <Input v-model="education.qualification" :placeholder="$t('educationTitle')" />
-      </label>
+      
+      <CustomLabel
+        :inputId="`school-${index}`"
+        :labelText="$t('school')"
+        :errorMessage="schoolError">
+        <Input
+          :id="`school-${index}`"
+          v-model="education.name"
+          :placeholder="$t('school')" />
+      </CustomLabel>
+      
+      <CustomLabel
+        :inputId="`from-${index}`"
+        :labelText="$t('from')"
+        :errorMessage="fromError">
+        <Input
+          :id="`from-${index}`"
+          v-model="education.from"
+          type="date"
+          :placeholder="$t('from')" />
+      </CustomLabel>
+      
+      <CustomLabel
+        :inputId="`to-${index}`"
+        :labelText="$t('to')"
+        :errorMessage="toError">
+        <Input
+          :id="`to-${index}`"
+          v-model="education.to"
+          type="date"
+          :placeholder="$t('to')" />
+      </CustomLabel>
+      
+      <CustomLabel
+        :inputId="`qualification-${index}`"
+        :labelText="$t('educationTitle')"
+        :errorMessage="qualificationError">
+        <Input
+          :id="`qualification-${index}`"
+          v-model="education.qualification"
+          :placeholder="$t('educationTitle')" />
+      </CustomLabel>
+      
       <button class="remove-button" @click="remove(education)">
         {{ $t('removeEducation') }}
       </button>
@@ -53,20 +106,41 @@
     </div>
 
     <form v-if="formType === 'project'" v-for="(project, index) in items" :key="project.id" @submit.prevent>
-      <p class="project-title">{{ $t('projectTitle') }} {{ index + 1 }}</p>
-      <label>
-        {{ $t('projectTitle') }}
-        <Input v-model="project.title" :placeholder="$t('projectTitle')" />
-      </label>
-      <label>
-        {{ $t('projectLink') }}
-        <Input v-model="project.link" :placeholder="$t('projectLink')" />
-      </label>
-      <label>
-        {{ $t('projectDescription') }}
-        <Input v-model="project.description" :placeholder="$t('projectDescription')" />
-      </label>
-      <button class="remove-button" @click="() => remove(project)">{{ $t('removeProject') }}</button>
+      <p class="project-title">{{ $t('projectTitle', { index: index + 1 }) }}</p>
+      
+      <CustomLabel
+        :inputId="`project-title-${index}`"
+        :labelText="$t('projectTitle')"
+        :errorMessage="projectTitleError">
+        <Input
+          :id="`project-title-${index}`"
+          v-model="project.title"
+          :placeholder="$t('projectTitle')" />
+      </CustomLabel>
+      
+      <CustomLabel
+        :inputId="`project-link-${index}`"
+        :labelText="$t('projectLink')"
+        :errorMessage="projectLinkError">
+        <Input
+          :id="`project-link-${index}`"
+          v-model="project.link"
+          :placeholder="$t('projectLink')" />
+      </CustomLabel>
+      
+      <CustomLabel
+        :inputId="`project-description-${index}`"
+        :labelText="$t('projectDescription')"
+        :errorMessage="projectDescriptionError">
+        <Input
+          :id="`project-description-${index}`"
+          v-model="project.description"
+          :placeholder="$t('projectDescription')" />
+      </CustomLabel>
+      
+      <button class="remove-button" @click="() => remove(project)">
+        {{ $t('removeProject') }}
+      </button>
     </form>
     <div v-if="formType === 'project'" class="container">
       <button @click="add('project')" class="center-button">{{ $t('addProject') }}</button>
@@ -76,6 +150,7 @@
 
 <script setup>
 import Input from "@/components/common/Input.vue";
+import CustomLabel from "@/components/common/CustomLabel.vue";
 
 const props = defineProps({
   items: Array,
